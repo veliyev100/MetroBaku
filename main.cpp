fb41523b27b81d9d1b672fd8917009d522e85fe4
Hamza Veliyev <hamzaveliyev00@gmail.com>
2025-03-13 17:40:59 +0400

    metro

--- main.cpp
+++ main.cpp
@@ -4,26 +4,27 @@
 #include <vector>
 #include <mutex>
 
-int travel_time = 1;
-int stop_time = 1;
+int travel_time = 4;
+int stop_time = 5;
 int turnaround_time = 4;
-int train_start_delay = 18;
+int train_start_delay = 6;
 int max_round = 2;
+int max_round_1 = 3;
 
 std::mutex cout_mutex;
 std::vector <std::string> Purpleline = {
-    "8 Noyabr,"
-    "Memar Ajami,"
-    "Avtovogzal,"
-    "Xochasan"
+        "8 Noyabr,"
+        "Memar Ajami,"
+        "Avtovogzal,"
+        "Xochasan"
 };
 std::vector <std::string> Redline = {
-    "Bakmil",
-    "Nariman Narimanov",
-    "Ganjlik",
-    "28 May",
-    "Sahil",
-    "Icherisheher"
+        "Bakmil",
+        "Nariman Narimanov",
+        "Ganjlik",
+        "28 May",
+        "Sahil",
+        "Icherisheher"
 };
 
 void RedmetroTrain(int id) {
@@ -76,8 +77,70 @@ void RedmetroTrain(int id) {
     }
 }
 
+std::vector <std::string> Purplelin = {
+        "8 Noyabr",
+        "Memar Ajami",
+        "Avtovogzal",
+        "Xochasan"
+};
+
+void PurplemetroTrain(int id) {
+    std::this_thread::sleep_for(std::chrono::seconds(id * train_start_delay));
+
+    for (int n = 0; n < max_round_1; n++) {
+        for (std::size_t i = 0; i < Purplelin.size(); i++) {
+            {
+                std::lock_guard<std::mutex> lock(cout_mutex);
+                std::cout << "Metro " << id << " - Train arrived at: " << Purplelin[i] << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(stop_time));
+
+            if (i < Purplelin.size() - 1) {
+                {
+                    std::lock_guard<std::mutex> lock(cout_mutex);
+                    std::cout << "Metro " << id << " - Train departing for " << Purplelin[i + 1] << "..." << std::endl;
+                }
+                std::this_thread::sleep_for(std::chrono::seconds(travel_time));
+            }
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(cout_mutex);
+            std::cout << "Metro " << id << " - Train turning around at " << Purplelin.back() << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(turnaround_time));
+
+        for (int i = static_cast<int>(Purplelin.size()) - 1; i >= 0; i--) {
+            {
+                std::lock_guard<std::mutex> lock(cout_mutex);
+                std::cout << "Metro " << id << " - Train arrived at: " << Purplelin[i] << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(stop_time));
+
+            if (i > 0) {
+                {
+                    std::lock_guard<std::mutex> lock(cout_mutex);
+                    std::cout << "Metro " << id << " - Train departing for " << Purplelin[i - 1] << "..." << std::endl;
+                }
+                std::this_thread::sleep_for(std::chrono::seconds(travel_time));
+            }
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(cout_mutex);
+            std::cout << "Metro " << id << " - Train turning around at " << Purplelin.front() << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(turnaround_time));
+    }
+}
+
+
 int main() {
     std::vector<std::thread> metros;
+
+    for (int a = 1; a <= 6; a++) {
+        metros.emplace_back(PurplemetroTrain, a);
+    }
 
     for (int i = 1; i <= 6; i++) {
         metros.emplace_back(RedmetroTrain, i);
