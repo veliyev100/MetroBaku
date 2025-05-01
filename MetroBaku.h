6205b92945e6d8925c916824540a79fe20744597
veliyev100 <hamzaveliyev00@gmail.com>
2025-05-02 00:09:07 +0400

    nadeyus zavtra sdam

--- MetroBaku.h
+++ MetroBaku.h
@@ -1,12 +1,10 @@
-#ifndef METROBAKU_H
-#define METROBAKU_H
-
+#pragma once
 #include <iostream>
 #include <thread>
 #include <chrono>
 #include <vector>
 #include <mutex>
-#include <fstream>  // Include for file output
+
 
 extern int travel_time;
 extern int stop_time;
@@ -18,15 +16,16 @@ extern int max_round_2;
 extern int max_round_3;
 
 extern std::mutex cout_mutex;
+extern std::mutex line_mutex;
+
 
 extern std::vector<std::string> Redline;
 extern std::vector<std::string> Purplelin;
 extern std::vector<std::string> Greenline;
 extern std::vector<std::string> Lightgreenline;
 
-void RedmetroTrain(int id, std::ofstream &outFile);
-void PurplemetroTrain(int id, std::ofstream &outFile);
-void GreenmetroTrain(int id, std::ofstream &outFile);
-void LightgreenTrainMetro(int id, std::ofstream &outFile);
 
-#endif
+void RedmetroTrain(int id);
+void PurplemetroTrain(int id);
+void GreenmetroTrain(int id);
+void LightgreenTrainMetro(int id);
